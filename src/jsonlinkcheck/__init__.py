d7eddbb4be6aa59613474b8afdc0c27ff7a220ec
Thomas Baer <thomas.baer@slub-dresden.de>
2025-01-09 14:16:40 +0100

    Vorbereitung für Pypi

---
+++ src/jsonlinkcheck/__init__.py
@@ -0,0 +1,11 @@
+"""
+JsonLinkCheck - Ein Tool zum Überprüfen und Bereinigen von URLs in JSON-Lines Dateien
+
+Copyright (c) 2024 SLUB Dresden - Thomas Baer
+"""
+
+__version__ = "0.1.0"
+
+from .main import main  # Exportiere die Hauptfunktion
+
+__all__ = ["main"]
--- src/jsonlinkcheck/main.py
+++ src/jsonlinkcheck/main.py
@@ -24,7 +24,6 @@ from typing import (
     List,
     Any,
     Optional,
-    Iterator,
     Set,
     DefaultDict,
     Tuple,
